1dcea0e0da4c1cd132164ff2fa4bdd91c652de86
Sherry Wu <shewu19@gmail.com>
2012-02-09 17:30:23 -0500

    updated readme and made sample java files more comprehensive

--- Point.java
+++ Point.java
@@ -1,5 +1,14 @@
 class Point {
 	int x;
 	int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public String toString() {
+		return "("+x+", "+y+")";
+	}
 }
 
--- Test.java
+++ Test.java
@@ -1,12 +1,18 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Test {
 	public
 		static
 	void main(String args[]) {
 		System.out.println("Hello");
+	 List<Point> pointList = new ArrayList<Point>();
+Point p = new Point(5, 3);
+	 	pointList.add(p);
+		String formattedString = " a  a\ta	a	a";
+		System.out.println("Here is a formatted string: "+formattedString);
+System.out.println(pointList);
+   System.exit(0);
 	}
 }
 
