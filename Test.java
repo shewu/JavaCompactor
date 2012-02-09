84fb87de4907cb82b3871758030ff3391a8fd494
Sherry Wu <shewu19@gmail.com>
2012-02-09 17:23:42 -0500

    working compressor, need to remove newlines

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Test {
+	public
+		static
+	void main(String args[]) {
+		System.out.println("Hello");
+	}
+}
+
--- jc.py
+++ jc.py
@@ -1,4 +1,74 @@
 #!/bin/python
 
 import sys
+import re
+
+class JavaProgram:
+	packages = set()
+	mainFile = ""
+	javaFiles = []
+
+	class JavaFile:
+		def __init__(self, name):
+			self.name = name
+			self.packages = set()
+
+			f = open(name)
+			self.src = f.read()
+			f.close()
+
+			# find main file
+			if re.search("public[ \t\r\n]+static[ \t\r\n]+void[ \t\r\n]+main", self.src) != None:
+				assert JavaProgram.mainFile == ""
+				JavaProgram.mainFile = self.name
+
+			# get and strip packages
+			self.findAndRemovePackages()
+
+			# TODO replace newlines
+
+		def __str__(self):
+			return self.name
+		
+		def findAndRemovePackages(self):
+			loc = self.src.find("import")
+			while loc != -1:
+				semicolon = self.src.find(";", loc)
+				assert semicolon != -1
+				JavaProgram.packages.add(self.src[loc:semicolon+1])
+				self.src = self.src[:loc] + self.src[semicolon+1:]
+				loc = self.src.find("import")
+
+		def getSource(self):
+			return self.src
+
+	def __init__(self):
+		pass
+	
+	def addFile(self, f):
+		self.javaFiles.append(self.JavaFile(f))
+	
+	def addFiles(self, l):
+		for f in l:
+			self.addFile(f)
+	
+	def writeFile(self):
+		if self.mainFile == '':
+			print "Cannot create file ''. Exiting."
+			return
+
+		f = open(self.mainFile+'.new', "w")
+
+		for p in self.packages:
+			f.write(p)
+
+		for jf in self.javaFiles:
+			f.write(jf.getSource())
+
+		f.close()
+		print "File " + self.mainFile + '.new successfully created'
+
+jp = JavaProgram()
+jp.addFiles(sys.argv[1:])
+jp.writeFile()
 
